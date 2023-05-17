2a6156d7a7f89ab4ed1816b3c4c58f9cb3821b9d
woo49m <i861128@gmail.com>
2023-05-17 16:31:50 +0800

    add assets from github

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import Logo from "../../assets/logo.svg";
 
 const Navbar = () => {
-  return <div>nacbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={Logo} alt="logo"></img>
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
